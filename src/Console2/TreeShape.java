fcef7f00cc90aaf58769540393c97756795842dc
Son <Sonntuet1997@gmail.com>
2017-11-13 10:17:30 +0700

    Bài tập lớn trí tuệ nhân tạo

--- src/Console2/TreeShape.java
+++ src/Console2/TreeShape.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 class TreeShape {
     public HashMap<PairShape, TreeShape> childs;
     public TreeShape parent;
-
+    public boolean isCenterEmpty;
     public TreeShape(HashMap<PairShape, TreeShape> childs, TreeShape parent) {
         this.childs = childs;
         this.parent = parent;
